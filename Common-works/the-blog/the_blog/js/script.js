5a2348ae8ff061612eddc09d62f97310166e1ca8
Amosov Vladislav <63251628+NIFUROXCAZID@users.noreply.github.com>
2024-06-08 17:12:19 +0300

    Delete Common-works/the-blog directory

--- Common-works/the-blog/the_blog/js/script.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-function scrollToElement(id) {
-    var element = document.getElementById(id);
-    if (element) {
-        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }
-}
